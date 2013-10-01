d30f9630877fb9975a2d27e5d93e85c3fd0fedd9
AbdealiJK <abdealikothari@gmail.com>
2013-10-01 15:43:38 +0530

    Added dice to events page 2

--- static/js/events/boxlayout.js
+++ static/js/events/boxlayout.js
@@ -39,7 +39,7 @@ function populate_event_group(category_name, dest){
         onclick_handler = "Dajaxice.events.show_event(Dajax.process,{'event_pk':'"+event_list[i].pk+"','event_name':'"+event_list[i].title.replace(/ /g, "~")+"','event_type':'"+event_list[i].event_type+"'})";
         
         dest.append("<div class='span3' id='event_no_"+event_list[i].pk+"'>"+
-                        "<div class='span12 title' onclick="+onclick_handler+"  id='event_no_"+event_list[i].pk+"_click'><span><h3>"+event_list[i].title+"</h3></span></div>"+
+                        "<div class='span12 title' onclick="+onclick_handler+"  id='event_no_"+event_list[i].pk+"_click'><span><h3>"+event_list[i].title+"</h3></span><br /><span class='white dice' style='opacity : 0.4'></span></div>"+
                         "<div class='span12 event_content'></div>"+
                     "</div>");
     }
