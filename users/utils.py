7e05b6e0b4f8e084bc0ed9db14b6c516b66e5dd9
gowtham-v <gowtham.vg.7@gmail.com>
2013-10-19 20:53:50 +0530

    Back functionality in dash

--- users/utils.py
+++ users/utils.py
@@ -6,11 +6,14 @@ from django.utils import timezone
 
 #This is a utility function, can be called from anywhere
 #returns all events that at a time can be registered
-def registrable_events(time=timezone.now()):
+def registrable_events(time=timezone.now(),user = None):
     #TODO:change erp
+    if user is None:
+        return None
     eventlist = []
     for event in ParticipantEvent.objects.using(erp_db).filter(registrable_online=True).filter(registration_ends > time).filter(registration_starts<time):
-        eventlist.append(event.title)
+        if event not in user.get_profile().get_regd_events():
+            eventlist.append(event.title)
     return eventlist
 
 
