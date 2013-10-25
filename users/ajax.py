559ebafe730e883542c8dade63b3a589fc88a341
gowtham-v <gowtham.vg.7@gmail.com>
2013-10-25 10:38:50 +0530

    add and remove team members complete

--- users/ajax.py
+++ users/ajax.py
@@ -172,7 +172,6 @@ def welcome(request):
         dajax.assign('#content_dash','innerHTML',html_stuff)
     return dajax.json()
     
-    
 @dajaxice_register
 def submit_tdp(request,teamevent_id = None,file_tdp=None):
     dajax = Dajax()
@@ -429,7 +428,7 @@ def login(request,login_form = None):
                 profile = UserProfile.objects.get(user=request.user)
                 context_dash_dict = {'profile':profile,'settings':settings}
                 html_stuff = render_to_string('dashboard/profile_view.html',context_dash_dict,RequestContext(request))
-                dajax.script("window.location.href=\"\";")
+                dajax.script("window.location.hash = \"dashboard\"")
                 if html_stuff:
                     dajax.assign('#content_dash','innerHTML',html_stuff)
 #                dajax.script('$(\"#list_user_head a\").click();')
