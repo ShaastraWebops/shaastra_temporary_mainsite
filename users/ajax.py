35feba70303565ce0f98f2f79a5c797585a1a305
Gowtham V <gowtham.vg.7@gmail.com>
2013-11-05 04:14:56 +0530

    redirect to events on logout, as logout is in #dashboard, and it shud be removed from hash on logout

--- users/ajax.py
+++ users/ajax.py
@@ -400,6 +400,7 @@ def logout(request,**kwargs):
     dajax.script('$.bootstrapGrowl("Successfully logged out!", {type:"success",delay:10000});' )
     dajax.assign("#login_logout", "innerHTML", '<a href="#login" onclick="$(\'#login\').modal(\'show\');">Login | Sign Up </a>')
     dajax.add_css_class("#dashboard","hide hidden")
+    dajax.script("window.location.hash = 'events';")
     return dajax.json()
     
 @dajaxice_register
