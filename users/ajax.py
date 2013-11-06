0ac785a4a1fbb544b3fe4b4c613e0defbd45986c
Shaastra <webops@shaastra.org>
2013-11-06 21:27:46 +0530

    Merging on server

--- users/ajax.py
+++ users/ajax.py
@@ -362,7 +362,7 @@ def show_event_tdp(request,teamevent_id=None):
             #dajax.script('$("#event_register").modal("show");')
     return dajax.json()
 
-
+from events.clean_coll import clean_coll
 @dajaxice_register
 def add_college(request,college=None,city=None,state=None):
     dajax = Dajax()
@@ -381,7 +381,11 @@ def add_college(request,college=None,city=None,state=None):
         dajax.script('$("#add_college").modal(\'hide\');')
         dajax.script('$("#login").show();')
         coll=College(name=college,city=city,state=state)
-        coll.save()
+        if clean_coll(coll.name) and clean_coll(coll.city):
+            coll.save()
+        else:
+            dajax.script('$.bootstrapGrowl("Please refrain from using profanity", {type:"danger",delay:10000});')
+            return dajax.json()
         dajax.assign("#add_coll_name",'innerHTML','%s'% college)
         dajax.assign("#add_coll_result",'innerHTML','College:')
         #dajax.script("$('#college')[0]value = '%s'" % college)
@@ -432,7 +436,7 @@ def login(request,login_form = None):
                 profile = UserProfile.objects.get(user=request.user)
                 context_dash_dict = {'profile':profile,'settings':settings}
                 html_stuff = render_to_string('dashboard/profile_view.html',context_dash_dict,RequestContext(request))
-                dajax.script("window.location.hash = \"dashboard\"")
+                dajax.script("window.location.hash = 'dashboard'")
                 if html_stuff:
                     dajax.assign('#content_dash','innerHTML',html_stuff)
 #                dajax.script('$(\"#list_user_head a\").click();')
