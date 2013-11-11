7da65714ca088179b7d1970d446dc13d8ccdf12d
AbdealiJK <abdealikothari@gmail.com>
2013-11-12 01:20:08 +0530

    Added back button in lectures

--- static/js/lectures/scripts.js
+++ static/js/lectures/scripts.js
@@ -16,14 +16,14 @@ function show_lecture(el) {
 
         // make other lectures smaller
         $(".lecture_items_div").addClass("offset");
-        $(".lecture_items_div .lecture_item").removeClass("span3");
+        $(".lecture_items_div .lecture_item").removeClass("span4");
         $(".lecture_items_div .lecture_item").addClass("span2");
         $(".lecture_items_div .extra").show()
         $(".lecture_items_div .span1_5").hide()
         
         $(".lecture_item").removeClass("active")
         item.addClass("active")
-        
+        $(".back_button_lectures").show(100);
     }
 }
 
@@ -31,12 +31,14 @@ function lecture_back() {
 
     $(".main_lecture_item").removeClass("lecture_item"); // tell main_lecture it has data to show
     $(".main_lecture_item > div > div").hide();
-    $(".main_lecture_item.lecture_item > div").hide().removeClass( 'expand' ); // Tell the section that it is small.
+    $(".main_lecture_item > div").hide().removeClass( 'expand' ); // Tell the section that it is small.
+    $(".main_lecture_item").html(""); // Tell the section that it is small.
     
     $(".lecture_items_div .span1_5").show();
     $(".lecture_items_div .extra").hide()
     $(".lecture_item").removeClass("active")
     $(".lecture_items_div .lecture_item").removeClass("span2");
-    $(".lecture_items_div .lecture_item").addClass("span3");
+    $(".lecture_items_div .lecture_item").addClass("span4");
     $(".lecture_carousel").show(500) 
+    $(".back_button_lectures").hide(100);
 }
